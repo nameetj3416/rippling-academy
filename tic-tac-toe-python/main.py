88ceae40b2310a5458d8da76a6988aeff7799bcd
Nameet Kumar Jain <nameetjain@Nameets-MacBook-Pro.local>
2022-07-08 13:50:17 +0530

    Game Loop feature added

--- tic-tac-toe-python/main.py
+++ tic-tac-toe-python/main.py
@@ -1,7 +1,15 @@
 from GameEngine import GameEngine
 
 def main():
-    game=GameEngine()
-    game.play()
+    user_key='c'
+    while(user_key!='q' and user_key!='Q'):
+        game=GameEngine()
+        game.play()
+        
+        print('To quit the game press Q or press any other key to continue to the next round.')
+        user_key=str(input())
+    
+    print('The Game Loop Ended!')    
+    
     
 main()
