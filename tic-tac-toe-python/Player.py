5f134a4fe050613a39e2162f436fcac74766fd0e
Nameet Kumar Jain <nameetjain@Nameets-MacBook-Pro.local>
2022-07-11 17:06:10 +0530

    Added few typing changes

--- tic-tac-toe-python/Player.py
+++ tic-tac-toe-python/Player.py
@@ -1,15 +1,16 @@
 class Player:
 
-    def __init__(self,player_symbol):
-        self.symbol=player_symbol
+    def __init__(self, player_symbol):
+        self.symbol = player_symbol
 
-    def validate_coordinates(self,x,y):
-        if x<=0 or y<=0 or x>3 or y>3:
+    @staticmethod
+    def validate_coordinates(x: int, y: int) -> bool:
+        if x <= 0 or y <= 0 or x > 3 or y > 3:
             return False
         return True
 
-    def move(self,game,x,y):
-        if self.validate_coordinates(x,y)==False:
-            return ('Invalid Coordinate! Choose a coordinate between [1,1] and [3,3]',0)
+    def move(self, game, x: int, y: int):
+        if not self.validate_coordinates(x, y):
+            return 'Invalid Coordinate! Choose a coordinate between [1,1] and [3,3]', 0
         else:
-            return game.change_cell(x,y,self.symbol)
+            return game.change_cell(x, y, self.symbol)
--- tic-tac-toe-python/main.py
+++ tic-tac-toe-python/main.py
@@ -1,15 +1,16 @@
-from GameEngine import GameEngine
+from game_engine import GameEngine
+
 
 def main():
-    user_key='c'
-    while(user_key!='q' and user_key!='Q'):
-        game=GameEngine()
+    user_key = 'c'
+    while user_key != 'q' and user_key != 'Q':
+        game = GameEngine()
         game.play()
-        
+
         print('To quit the game press Q or press any other key to continue to the next round.')
-        user_key=str(input())
-    
-    print('The Game Loop Ended!')    
-    
-    
+        user_key = str(input())
+
+    print('The Game Loop Ended!')
+
+
 main()
